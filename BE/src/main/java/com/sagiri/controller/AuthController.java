00909b4a5992fbc5336037380e3639663690d18d
sagiri <sagiri0512@qq.com>
2026-07-01 01:41:25 +0800

    修正Bug ： 状态码始终是200，增加了status=result.getCode()

--- BE/src/main/java/com/sagiri/controller/AuthController.java
+++ BE/src/main/java/com/sagiri/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.sagiri.controller;
 import com.sagiri.common.Result;
 import com.sagiri.dto.UserLogin;
 import com.sagiri.service.EmployeeService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,8 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public Result<?> login(@RequestBody UserLogin userLogin){
-        return employeeService.login(userLogin.getEmpNo(), userLogin.getPassword());
+    public ResponseEntity<Result<?>> login(@RequestBody UserLogin userLogin){
+        Result<?> result = employeeService.login(userLogin.getEmpNo(), userLogin.getPassword());
+        return ResponseEntity.status(result.getCode()).body(result);
     }
 }
