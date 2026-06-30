4c1b6642c83a35364fe5d5442088c4a1b59919d0
sagiri <sagiri0512@qq.com>
2026-07-01 01:02:24 +0800

    RV对应

--- BE/src/main/java/com/sagiri/controller/AuthController.java
+++ BE/src/main/java/com/sagiri/controller/AuthController.java
@@ -3,7 +3,6 @@ package com.sagiri.controller;
 import com.sagiri.common.Result;
 import com.sagiri.dto.UserLogin;
 import com.sagiri.service.EmployeeService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
